5195bfb3f2a0aa1ee99db85aef7c8e7e4dfbd9e9
@v.igumnov <v.igumnov@mashroom.live>
2024-01-30 17:46:11 +0300

    chapter 4

---
+++ vue-faq-modal-plugin/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, inject } from "vue"
+
+const modals = inject("$modals")
+const result = ref("")
+
+function showModal() {
+    result.value = "";
+    modals.show("myModal").then(() => {
+        result.value = "Modal accepted"
+    }, () => {
+        result.value = "Modal cancelled"
+    })
+}
+
+</script>
+
+<template>
+    <div class="app">
+        <button @click="showModal">
+            Show modal
+        </button>
+        <div>{{result}}</div>
+        <Modal name="myModal" title="Modal example">
+            Some important content here
+        </Modal>
+    </div>
+</template>
+
+<style scoped>
+.app {
+    width: 100vw;
+    min-height: 100vh;
+    padding: 0;
+}
+</style>
